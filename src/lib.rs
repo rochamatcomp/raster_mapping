5709a9cc6b45d61c4477e7b36ec0b3207fc077c6
André Rocha <rocha.matcomp@gmail.com>
2018-01-01 23:26:37 +0100

    Changing &str to String.

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,14 @@ pub struct Raster<T>{
 }
 
 pub trait Mapping<TypeData>{
-    fn get_data(filename: &str) -> TypeData;
-    fn new(filename: &str) -> Self;
-    fn algebra(maps: HashMap<&str, f32>) -> Self;
+    fn get_data(filename: &String) -> TypeData;
+    fn new(filename: String) -> Self;
+    fn algebra(maps: HashMap<String, f32>) -> Self;
 }
 
 impl Mapping<Array2<f32>> for Raster<f32>
 {
-    fn get_data(filename: &str) -> Array2<f32>{
+    fn get_data(filename: &String) -> Array2<f32>{
         let path = Path::new(filename);
         let dataset = Dataset::open(path).unwrap();
         let shape = dataset.size();
@@ -36,13 +36,13 @@ impl Mapping<Array2<f32>> for Raster<f32>
         Array2::from_shape_vec((rows, cols), buffer.data).unwrap()
     }
     
-    fn new(filename: &str) -> Self{
+    fn new(filename: String) -> Self{
         Raster::<f32>{
-            data: Self::get_data(filename)
+            data: Self::get_data(&filename)
         }
     }
     
-    fn algebra(maps: HashMap<&str, f32>) -> Self{
+    fn algebra(maps: HashMap<String, f32>) -> Self{
         let mut data: Array2<f32>;
         let mut weight: f32;
         let mut result: Array2<f32> = array![[]];
@@ -133,7 +133,7 @@ mod test_approx {
 
     #[test]
     fn test_abs_diff_eq(){
-        let raster1 = Raster::new("data/data1.asc");
+        let raster1 = Raster::new("data/data1.asc".to_string());
         let raster2 = Raster::<f32>{
             data: Array2::from_shape_vec((3, 4), vec![0.388889, 0.513889, 0.638889, 0.805556, 0.444447, 0.583333, 0.680556, -32768.0, 0.625, 0.625, -32768.0, -32768.0]).unwrap()
         };
@@ -143,7 +143,7 @@ mod test_approx {
 
     #[test]
     fn test_relative_eq(){
-        let raster1 = Raster::new("data/data1.asc");
+        let raster1 = Raster::new("data/data1.asc".to_string());
         let raster2 = Raster::<f32>{
             data: Array2::from_shape_vec((3, 4), vec![0.388889, 0.513889, 0.638889, 0.805556, 0.444447, 0.583333, 0.680556, -32768.0, 0.625, 0.625, -32768.0, -32768.0]).unwrap()
         };
@@ -153,7 +153,7 @@ mod test_approx {
 
     #[test]
     fn test_ulps_eq(){
-        let raster1 = Raster::new("data/data1.asc");
+        let raster1 = Raster::new("data/data1.asc".to_string());
         let raster2 = Raster::<f32>{
             data: Array2::from_shape_vec((3, 4), vec![0.388889, 0.513889, 0.638889, 0.805556, 0.444447, 0.583333, 0.680556, -32768.0, 0.625, 0.625, -32768.0, -32768.0]).unwrap()
         };
@@ -173,11 +173,11 @@ mod test_algebra {
         let weight3: f32 = 0.2;
         let weight4: f32 = 0.2;
         
-        let raster1 = Raster::new("data/data1.asc");
-        let raster2 = Raster::new("data/data2.asc");
-        let raster3 = Raster::new("data/data3.asc");
-        let raster4 = Raster::new("data/data4.asc");
-        let result = Raster::new("data/result.asc");
+        let raster1 = Raster::new("data/data1.asc".to_string());
+        let raster2 = Raster::new("data/data2.asc".to_string());
+        let raster3 = Raster::new("data/data3.asc".to_string());
+        let raster4 = Raster::new("data/data4.asc".to_string());
+        let result = Raster::new("data/result.asc".to_string());
 
         let combination = Raster::<f32>{
             data: weight1 * raster1.data + weight2 * raster2.data + weight3 * raster3.data + weight4 * raster4.data
@@ -189,14 +189,14 @@ mod test_algebra {
 
     #[test]
     fn test_new_algebra(){
-        let mut maps: HashMap<&str, f32> = HashMap::new();
+        let mut maps: HashMap<String, f32> = HashMap::new();
 
-        maps.insert("data/data1.asc", 0.4);
-        maps.insert("data/data2.asc", 0.2);
-        maps.insert("data/data3.asc", 0.2);
-        maps.insert("data/data4.asc", 0.2);
+        maps.insert("data/data1.asc".to_string(), 0.4);
+        maps.insert("data/data2.asc".to_string(), 0.2);
+        maps.insert("data/data3.asc".to_string(), 0.2);
+        maps.insert("data/data4.asc".to_string(), 0.2);
         
-        let result = Raster::new("data/result.asc");
+        let result = Raster::new("data/result.asc".to_string());
         let combination = Raster::algebra(maps);
 
         assert_relative_eq!(combination, result, epsilon = 1e-5f32);
@@ -204,7 +204,7 @@ mod test_algebra {
 
     #[test]
     fn test_new_algebra_zero(){
-        let maps: HashMap<&str, f32> = HashMap::new();
+        let maps: HashMap<String, f32> = HashMap::new();
         let empty: Array2<f32> = array![[]];
         let result = Raster::<f32>{data: empty};
         let combination = Raster::algebra(maps);
